96c85fbea64cea75c79b5da399c74bcce1bd863c
Cristi Boboc <cristi@cbsoft.ro>
2020-03-23 16:39:59 +0200

    minor fixes

--- application/frontend/views.py
+++ application/frontend/views.py
@@ -6,15 +6,16 @@ from frontend.models import SubscriberValidation
 def index(request):
     context = {}
     context['boards'] = Board.objects.all()[:6]
-
+    if request.method == 'POST':
+        context = {'result': 0}
     return render(request, 'frontend/index.html', context)
 
 
 def validate_email(request, uuid):
-    context = {'result': False}
+    context = {'result': 1}
     validation = SubscriberValidation.objects.filter(uuid=uuid).first()
     if validation:
-        context = {'result': True}
+        context = {'result': 2}
         if not validation.subscriber.email_validated:
             validation.subscriber.email_validated = True
             validation.subscriber.save()
