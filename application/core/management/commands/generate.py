a2f3a9e8787128254b7a5bee68713797091f0fee
Cristi Boboc <cristi@cbsoft.ro>
2020-03-23 16:08:43 +0200

    minor fixes

--- application/core/management/commands/generate.py
+++ application/core/management/commands/generate.py
@@ -1,9 +1,8 @@
 import os, datetime
 
-from PyPDF2 import PdfFileMerger, PdfFileReader
+from PyPDF2 import PdfFileMerger
 from django.conf import settings
-from django.core.files import File
-from django.core.management.base import BaseCommand, CommandError
+from django.core.management.base import BaseCommand
 
 from core.models import Board
 
